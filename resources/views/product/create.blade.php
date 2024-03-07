d8ee108f9ca31985884533d0e57168b1a2131563
Alexandre <hallysson@adgency.com.br>
2024-03-07 11:34:42 -0300

    working in progress create product

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base')
 @section('title','Publicar')
-@vite(['resources/js/scripts/alpine.js','resources/js/scripts/quill.js', 'resources/js/scripts/dropzone.js'])
+@vite(['resources/js/scripts/alpine.js'])
 
 @section('meta-data')
 
@@ -73,195 +73,26 @@
             </div>
       </div>
       
-      <div x-show="secondStep" x-init="$watch('secondStep', value => initDropzone() )" class=" w-full p-2 flex justify-center" x-transition x-data="{ brands : getAllPhoneBrands(), currentBrand : '', brandsmodels : getBrandsAndModels()}">
-        <form action="{{ route('product.create')  }}" method="POST" enctype="multipart/form-data">
-          @csrf
-          <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
-              <h2 class="text-base font-semibold leading-7 text-gray-900">Detalhes do Produto</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-600">Essas informações será mostrada publicamente, então tenha cuidado com o que digitar.</p>
-        
-              <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6" >
-                <div class="sm:col-span-4">
-                  <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Nome</label>
-                  <div class="mt-2">
-                    <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      {{-- <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> --}}
-                      <input type="text" name="name" id="username" autocomplete="off" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Ex: IPhone PRO MAX">
-                    </div>
-                  </div>
-                </div>
-        
-                <div class="col-span-full mb-32">
-                  <label for="about" class="block mb-4 text-sm font-medium leading-6 text-gray-900">Decrição (obrigatório)</label>
-                  <div class="mt-2" id="editor">
-                    <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-                  </div>
-                  <p class="mt-3 text-sm leading-6 text-gray-600">Faça uma descrição do seu produto.</p>
-                </div>
+      <div x-show="secondStep">
 
-                <div class="col-span-full">
-                  <label class="text-sm mb-24 font-medium leading-6 text-gray-900">Imagens (obrigatório)</label>
-                  <div class="dropzone-preview"></div>
-                  <div class="dropzone upload-zone dz-preview dz-file-preview" 
-                      data-url="{{ route('product.store') }}"
-                      data-accepted-files="image/*"
-                      data-csrf-token="{{ csrf_token() }}"
-                      id="dropzone">
-                      <img data-dz-thumbnail />
-                      <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
-                      
-                    <div class="dz-message" data-dz-message>
-                      <span class="dz-message-text">Arraste e solte arquivos</span>
-                      <span class="dz-message-or">ou</span>
-                      <button class="btn btn-primary">SELECIONE</button>
-                  </div>
-                  </div>
-                </div>
-                
+        
+          <div class="grid lg:grid-cols-3 grid-cols-2 place-items-center  items-center gap-4 content-center">
+      
+            @foreach(range(1,12) as $key => $value)
+            
+              <div class="max-w-sm p-6 cursor-pointer bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition ease-in-out hover:-translate-y-1 hover:scale-110">
+                <img src="" alt="">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Need a help in Claim?</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+              </div>
 
-              </div>
-            </div>
+            @endforeach
         
-            <div class="border-b border-gray-900/10 pb-12">
-              <h2 class="text-base font-semibold leading-7 text-gray-900">Mais algumas informações</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-600">Informações referente ao estado do produto e suas características.</p>
+        </div>
         
-              <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                <div class="sm:col-span-3">
-                  <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Marca</label>
-                  <div class="mt-2">
-                    <select id="brand" name="brand" x-model="currentBrand" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                      <template x-for="brand in brands">
-                        <option x-text="brand"></option>
-                      </template>
-                    </select>
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-3">
-                  <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Modelo</label>
-                  <div class="mt-2">
-                    <select id="model" name="brand" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                      <template x-for="brandmodel in brandsmodels">
-                        <option x-text="brandmodel.name == currentBrand ? brandmodel.models : ''"></option>
-                      </template>
-                    </select>
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-4">
-                  <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                  <div class="mt-2">
-                    <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-3">
-                  <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
-                  <div class="mt-2">
-                    <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>
-                  </div>
-                </div>
-        
-                <div class="col-span-full">
-                  <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-                  <div class="mt-2">
-                    <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-2 sm:col-start-1">
-                  <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
-                  <div class="mt-2">
-                    <input type="text" name="city" id="city" autocomplete="address-level2" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-2">
-                  <label for="region" class="block text-sm font-medium leading-6 text-gray-900">State / Province</label>
-                  <div class="mt-2">
-                    <input type="text" name="region" id="region" autocomplete="address-level1" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-        
-                <div class="sm:col-span-2">
-                  <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
-                  <div class="mt-2">
-                    <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                  </div>
-                </div>
-              </div>
-            </div>
-        
-            <div class="border-b border-gray-900/10 pb-12">
-              <h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-600">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
-        
-              <div class="mt-10 space-y-10">
-                <fieldset>
-                  <legend class="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-                  <div class="mt-6 space-y-6">
-                    <div class="relative flex gap-x-3">
-                      <div class="flex h-6 items-center">
-                        <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      </div>
-                      <div class="text-sm leading-6">
-                        <label for="comments" class="font-medium text-gray-900">Comments</label>
-                        <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                      </div>
-                    </div>
-                    <div class="relative flex gap-x-3">
-                      <div class="flex h-6 items-center">
-                        <input id="candidates" name="candidates" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      </div>
-                      <div class="text-sm leading-6">
-                        <label for="candidates" class="font-medium text-gray-900">Candidates</label>
-                        <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
-                      </div>
-                    </div>
-                    <div class="relative flex gap-x-3">
-                      <div class="flex h-6 items-center">
-                        <input id="offers" name="offers" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      </div>
-                      <div class="text-sm leading-6">
-                        <label for="offers" class="font-medium text-gray-900">Offers</label>
-                        <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                      </div>
-                    </div>
-                  </div>
-                </fieldset>
-                <fieldset>
-                  <legend class="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-                  <p class="mt-1 text-sm leading-6 text-gray-600">These are delivered via SMS to your mobile phone.</p>
-                  <div class="mt-6 space-y-6">
-                    <div class="flex items-center gap-x-3">
-                      <input id="push-everything" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      <label for="push-everything" class="block text-sm font-medium leading-6 text-gray-900">Everything</label>
-                    </div>
-                    <div class="flex items-center gap-x-3">
-                      <input id="push-email" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      <label for="push-email" class="block text-sm font-medium leading-6 text-gray-900">Same as email</label>
-                    </div>
-                    <div class="flex items-center gap-x-3">
-                      <input id="push-nothing" name="push-notifications" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600">
-                      <label for="push-nothing" class="block text-sm font-medium leading-6 text-gray-900">No push notifications</label>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-            </div>
-          </div>
-        
-          <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Criar</button>
-          </div>
-        </form>
+      
         
       </div>
 
@@ -271,169 +102,5 @@
     
 @endsection
 
-@section('scripts')
 
-<script>
-  function getAllPhoneBrands(){
-      return [
-        "Apple",
-          "Samsung",
-          "Xiaomi",
-          "Motorola",
-          "Sony",
-          "Asus",
-      ];
-  }
-
-  function getBrandsAndModels() {
-    return [
-        getAppleModels(),
-        getSamsungModels(),
-        getMotorolaModels(),
-        getSonyModels(),
-        getNokiaModels(),
-        getAsusModels(),
-        getXiaomiModels()
-    ];
-}
-
-  function getAppleModels() {
-    return {
-        name: "Apple",
-        models: [
-            "iPhone 13",
-            "iPhone 12",
-            "iPhone SE (2020)",
-            "iPhone 11",
-            "iPhone XR",
-            "iPhone 8",
-            "iPhone 7",
-            "iPhone SE",
-            "iPhone XS",
-            "iPhone X"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Samsung
-function getSamsungModels() {
-    return {
-        name: "Samsung",
-        models: [
-            "Galaxy S21",
-            "Galaxy S20",
-            "Galaxy Note 20",
-            "Galaxy A52",
-            "Galaxy A32",
-            "Galaxy S21 Ultra",
-            "Galaxy Note 10",
-            "Galaxy Z Fold 3",
-            "Galaxy Z Flip 3",
-            "Galaxy A12"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Motorola
-function getMotorolaModels() {
-    return {
-        name: "Motorola",
-        models: [
-            "Moto G Power (2021)",
-            "Moto G Stylus (2021)",
-            "Moto G9 Plus",
-            "Moto G Fast",
-            "Moto E7 Plus",
-            "Moto G Power (2020)",
-            "Moto G Stylus (2020)",
-            "Moto G Pro",
-            "Moto G8 Plus",
-            "Moto G8 Power"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Sony
-function getSonyModels() {
-    return {
-        name: "Sony",
-        models: [
-            "Sony Xperia 1 III",
-            "Sony Xperia 5 III",
-            "Sony Xperia 1 II",
-            "Sony Xperia 5 II",
-            "Sony Xperia 10 III",
-            "Sony Xperia 10 II",
-            "Sony Xperia L4",
-            "Sony Xperia XZ3",
-            "Sony Xperia XZ2",
-            "Sony Xperia XA2"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Nokia
-function getNokiaModels() {
-    return {
-        name: "Nokia",
-        models: [
-            "Nokia 9 PureView",
-            "Nokia 8.3 5G",
-            "Nokia 7.2",
-            "Nokia 6.2",
-            "Nokia 5.3",
-            "Nokia 3.4",
-            "Nokia 2.4",
-            "Nokia 1.4",
-            "Nokia 5.4",
-            "Nokia 3.1 Plus"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Asus
-function getAsusModels() {
-    return {
-        name: "Asus",
-        models: [
-            "Asus ZenFone 8",
-            "Asus ZenFone 7 Pro",
-            "Asus ROG Phone 5",
-            "Asus ZenFone 6",
-            "Asus ZenFone 5Z",
-            "Asus ZenFone Max Pro (M2)",
-            "Asus ZenFone Max Plus (M1)",
-            "Asus ZenFone Max (M2)",
-            "Asus ZenFone Live",
-            "Asus ZenFone AR"
-        ]
-    };
-}
-
-// Função para obter modelos de celulares da Xiaomi
-function getXiaomiModels() {
-    return {
-        name: "Xiaomi",
-        models: [
-            "Xiaomi Mi 11",
-            "Xiaomi Mi 10",
-            "Xiaomi Redmi Note 10",
-            "Xiaomi Redmi Note 9",
-            "Xiaomi Redmi 9",
-            "Xiaomi Poco X3",
-            "Xiaomi Mi 10T",
-            "Xiaomi Mi Note 10",
-            "Xiaomi Redmi Note 8",
-            "Xiaomi Mi 9"
-        ]
-    };
-}
-
-
-
-  
-
-</script>
-
-@endsection
 
